c5dc2c83360d880e6bd4ad12686fac54f063f435
Onu Daniel Onyebuchi <onudanielonyebuchi@gmail.com>
2025-06-18 18:02:58 +0100

    Create Ownable.sol

---
+++ contracts/product/Ownable.sol
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.22;
+
+import "@openzeppelin/contracts/utils/Context.sol";
+
+/**
+ * @dev copy of "@openzeppelin/contracts/access/Ownable.sol" bc we do need onlyOwner to be virtual
+
+ */
+abstract contract Ownable is Context {
+    address private _owner;
+    address private _pendingOwner;
+
+    event OwnershipTransferred(
+        address indexed previousOwner,
+        address indexed newOwner
+    );
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor() {
+        _transferOwnership(_msgSender());
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() virtual {
+        require(owner() == _msgSender(), "Ownable: caller is not the owner");
+        _;
+    }
+
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions anymore. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby removing any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public virtual onlyOwner {
+        _transferOwnership(address(0));
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     * Uses 2-step ownership transfer pattern:
+     */
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(
+            newOwner != address(0),
+            "Ownable: new owner is the zero address"
+        );
+        _pendingOwner = newOwner;
+
+        _transferOwnership(newOwner);
+    }
+
+    function claimOwnership() public {
+        require(msg.sender == _pendingOwner, "Not pending owner");
+        _transferOwnership(_pendingOwner);
+        _pendingOwner = address(0);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Internal function without access restriction.
+     */
+    function _transferOwnership(address newOwner) internal virtual {
+        address oldOwner = _owner;
+        _owner = newOwner;
+        emit OwnershipTransferred(oldOwner, newOwner);
+    }
+}
